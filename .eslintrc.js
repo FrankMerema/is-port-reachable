f08fb29f9794bee553d6698d37254dd0275f6102
Frank Merema <frankmerema@gmail.com>
2020-06-02 13:44:51 +0200

    chore(lint): lix linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,10 @@ module.exports = {
     '@typescript-eslint/adjacent-overload-signatures': 'error',
     '@typescript-eslint/array-type': 'error',
     '@typescript-eslint/ban-types': 'error',
-    '@typescript-eslint/class-name-casing': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: 'class', format: ['PascalCase'] }
+    ],
     '@typescript-eslint/consistent-type-assertions': 'error',
     '@typescript-eslint/explicit-member-accessibility': [
       'off',
